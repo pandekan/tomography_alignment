bfe34b4640457a8610f093f10e0f9061dfe35477
Kanupriya Pande <kanupriya.pande@desy.de>
2021-12-01 19:32:22 -0800

    module for geometry, phantom, projection operator, etc.

---
+++ utilities/generate_phantom.py
@@ -0,0 +1,229 @@
+"""
+Module for generating shepp-logan phantom and
+phantom with arbitrary number of ellipsoids with arbitrary parameters.
+
+Most of these modules are from tomopy by Gursoy et al.
+"""
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import numpy as np
+
+
+def _totuple(size, dim):
+    """
+    Converts size to tuple.
+    """
+    if not isinstance(size, tuple):
+        if dim == 2:
+             size = (size, size)
+        elif dim == 3:
+             size = (size, size, size)
+             
+    return size
+    
+
+def shepp3d(size=128, dtype='float32'):
+    """
+    Load 3D Shepp-Logan image array.
+
+    Parameters
+    ----------
+    size : int or tuple, optional
+        Size of the 3D data.
+    dtype : str, optional
+        The desired data-type for the array.
+
+    Returns
+    -------
+    ndarray
+        Output 3D test image.
+    """
+    size = _totuple(size, 3)
+    shepp_params = _array_to_params(_get_shepp_array())
+    return phantom(size, shepp_params, dtype).clip(0, np.inf)
+
+
+def arbitrary_phantom(size=128, n_features=20, dtype='float32'):
+    """
+    Create an arbitrary phantom with n ellipsoids.
+    Parameters for ellipsoids are randomly generated.
+    
+    size: int or tuple giving size of the 3D phantom
+    n_features: number of ellipsoids
+    dtype: desired datatype for the array
+    
+    Returns:
+    ndarray: 3D phantom
+    """
+    
+    size = _totuple(size, 3)
+    # ellipsoid parameters
+    phantom_params = np.zeros((n_features, 10))
+    
+    # get voxel values between -1 and 1
+    phantom_params[:, 0] = np.random.randint(-100, 100, n_features)/100.
+    
+    # centers of ellipsoids between 0 and 1
+    phantom_params[:, 1:4] = np.random.rand(n_features, 3)
+    
+    # a, b, c parameters between -1 and 1
+    phantom_params[:, 4:7] = (np.random.randint(-200, 200, n_features*3)/200).reshape(n_features, 3)
+    
+    # alpha, beta, phi in degrees between 0 and 180
+    phantom_params[:, 7:] = np.rad2deg(np.random.rand(n_features, 3)*np.pi)
+    
+    return phantom(size, _array_to_params(phantom_params), dtype).clip(0., np.inf)
+
+    
+def phantom(size, params, dtype='float32'):
+    """
+    Generate a cube of given size using a list of ellipsoid parameters.
+
+    Parameters
+    ----------
+    size: tuple of int
+        Size of the output cube.
+    params: list of dict
+        List of dictionaries with the parameters defining the ellipsoids
+        to include in the cube.
+    dtype: str, optional
+        Data type of the output ndarray.
+
+    Returns
+    -------
+    ndarray
+        3D object filled with the specified ellipsoids.
+    """
+    # instantiate ndarray cube
+    obj = np.zeros(size, dtype=dtype)
+
+    # define coords
+    coords = _define_coords(size)
+
+    # recursively add ellipsoids to cube
+    for param in params:
+        _ellipsoid(param, out=obj, coords=coords)
+    return obj
+
+
+def _ellipsoid(params, shape=None, out=None, coords=None):
+    """
+    Generate a cube containing an ellipsoid defined by its parameters.
+    If out is given, fills the given cube instead of creating a new one.
+    """
+    # handle inputs
+    if shape is None and out is None:
+        raise ValueError("You need to set shape or out")
+    if out is None:
+        out = np.zeros(shape)
+    if shape is None:
+        shape = out.shape
+    if len(shape) == 1:
+        shape = shape, shape, shape
+    elif len(shape) == 2:
+        shape = shape[0], shape[1], 1
+    elif len(shape) > 3:
+        raise ValueError("input shape must be lower or equal to 3")
+    if coords is None:
+        coords = _define_coords(shape)
+
+    # rotate coords
+    coords = _transform(coords, params)
+
+    # recast as ndarray
+    coords = np.asarray(coords)
+    np.square(coords, out=coords)
+    ellip_mask = coords.sum(axis=0) <= 1.
+    ellip_mask.resize(shape)
+
+    # fill ellipsoid with value
+    out[ellip_mask] += params['A']
+    return out
+
+
+def _rotation_matrix(p):
+    """
+    Defines an Euler rotation matrix from angles phi, theta and psi.
+    """
+    cphi = np.cos(np.radians(p['phi']))
+    sphi = np.sin(np.radians(p['phi']))
+    ctheta = np.cos(np.radians(p['theta']))
+    stheta = np.sin(np.radians(p['theta']))
+    cpsi = np.cos(np.radians(p['psi']))
+    spsi = np.sin(np.radians(p['psi']))
+    alpha = [[cpsi * cphi - ctheta * sphi * spsi,
+              cpsi * sphi + ctheta * cphi * spsi,
+              spsi * stheta],
+             [-spsi * cphi - ctheta * sphi * cpsi,
+              -spsi * sphi + ctheta * cphi * cpsi,
+              cpsi * stheta],
+             [stheta * sphi,
+              -stheta * cphi,
+              ctheta]]
+    return np.asarray(alpha)
+
+
+def _define_coords(shape):
+    """
+    Generate a tuple of coords in 3D with a given shape.
+    """
+    mgrid = np.lib.index_tricks.nd_grid()
+    cshape = np.asarray(1j) * shape
+    x, y, z = mgrid[-1:1:cshape[0], -1:1:cshape[1], -1:1:cshape[2]]
+    return x, y, z
+
+
+def _transform(coords, p):
+    """
+    Apply rotation, translation and rescaling to a 3-tuple of coords.
+    """
+    alpha = _rotation_matrix(p)
+    out_coords = np.tensordot(alpha, coords, axes=1)
+    _shape = (3,) + (1,) * (out_coords.ndim - 1)
+    _dt = out_coords.dtype
+    M0 = np.array([p['x0'], p['y0'], p['z0']], dtype=_dt).reshape(_shape)
+    sc = np.array([p['a'], p['b'], p['c']], dtype=_dt).reshape(_shape)
+    out_coords -= M0
+    out_coords /= sc
+    return out_coords
+
+
+def _get_shepp_array():
+    """
+    Returns the parameters for generating modified Shepp-Logan phantom.
+    """
+    shepp_array = [
+        [1.,  .6900, .920, .810,   0.,     0.,   0.,   90.,   90.,   90.],
+        [-.8, .6624, .874, .780,   0., -.0184,   0.,   90.,   90.,   90.],
+        [-.2, .1100, .310, .220,  .22,     0.,   0., -108.,   90.,  100.],
+        [-.2, .1600, .410, .280, -.22,     0.,   0.,  108.,   90.,  100.],
+        [.1,  .2100, .250, .410,   0.,    .35, -.15,   90.,   90.,   90.],
+        [.1,  .0460, .046, .050,   0.,     .1,  .25,   90.,   90.,   90.],
+        [.1,  .0460, .046, .050,   0.,    -.1,  .25,   90.,   90.,   90.],
+        [.1,  .0460, .023, .050, -.08,  -.605,   0.,   90.,   90.,   90.],
+        [.1,  .0230, .023, .020,   0.,  -.606,   0.,   90.,   90.,   90.],
+        [.1,  .0230, .046, .020,  .06,  -.605,   0.,   90.,   90.,   90.]]
+    return shepp_array
+
+
+def _array_to_params(array):
+    """
+    Converts list to a dictionary.
+    """
+    # mandatory parameters to define an ellipsoid
+    params_tuple = [
+        'A',
+        'a', 'b', 'c',
+        'x0', 'y0', 'z0',
+        'phi', 'theta', 'psi']
+
+    array = np.asarray(array)
+    out = []
+    for i in range(array.shape[0]):
+        tmp = dict()
+        for k, j in zip(params_tuple, list(range(array.shape[1]))):
+            tmp[k] = array[i, j]
+        out.append(tmp)
+    return out
---
+++ utilities/geometry.py
@@ -0,0 +1,100 @@
+import numpy as np
+
+
+class Geometry(object):
+    """
+    Detector and object setup for parallel beam geometry.
+    """
+    
+    def __init__(self, n_proj, voxel_shape, voxel_pixsize, detector_shape, detector_pixsize,
+                 cor_shift=None, step_size=1.0):
+        """
+        :param n_proj: int, number of projections
+        :param voxel_shape: int, (3,)
+        :param voxel_pixsize: float (3,)
+        :param detector_shape: int (2,)
+        :param det_pixsize: float (2,)
+        """
+        self.n_proj = n_proj
+        self.vox_shape = voxel_shape
+        self.vox_pix = voxel_pixsize
+        self.vox_size = self.vox_shape * self.vox_pix
+        self.n_vox = np.prod(self.vox_shape)
+        self.det_shape = detector_shape
+        self.det_pix = detector_pixsize
+        self.det_size = self.det_shape * self.det_pix
+        self.n_det = np.prod(self.det_shape)
+        self.vox_ds = np.array([1, 1, 1])
+        if cor_shift is None:
+            self.cor_shift = np.zeros((n_proj, 3))
+        else:
+            if len(cor_shift.shape) == 2:
+                assert(cor_shift.shape[0] == n_proj)
+                assert(cor_shift.shape[1] == 3)
+                self.cor_shift = cor_shift
+            elif len(cor_shift.shape) == 1:
+                assert(np.size(cor_shift) == 3)
+                self.cor_shift = np.tile(cor_shift, n_proj).reshape(n_proj, 3)
+            else:
+                print('shape or size of cor_shift not valid')
+            
+        self.step_size = step_size
+        self._voxel_detector_grid()
+        
+    def _geo_parameters(self, angles=None, shifts=None):
+        """
+        :param angles: float (3, n_proj) (tomo, alpha, beta)
+        :param shifts: xyz translations
+        :return: self.angles and self.shifts
+        """
+        if angles is None:
+            self.angles = np.zeros((3, self.n_proj))
+            self.angles[0] = np.linspace(0., np.pi, self.n_proj)
+        elif len(angles.shape) == 1:
+            assert(np.size(angles) == self.n_proj)
+            self.angles = np.zeros((3, self.n_proj))
+            self.angles[0] = angles
+        else:
+            assert(angles.shape[1] == self.n_proj)
+            self.angles = np.zeros((3, self.n_proj))
+            self.angles[0] = angles[0]
+            self.angles[1] = angles[1]
+            if angles.shape[0] == 3:
+                self.angles[2] = angles[2]
+        
+        if shifts is None:
+            self.shifts = np.zeros((3, self.n_proj))
+        else:
+            assert(shifts.shape[0] == 3)
+            assert(shifts.shape[1] == self.n_proj)
+            self.shifts = shifts
+    
+    def _voxel_detector_grid(self):
+        
+        # first set up voxel centers and origin
+        nx, ny, nz = self.vox_shape
+        sx, sy, sz = self.vox_size
+        x = np.linspace(-sx/2, sx/2, nx, endpoint=False) + 0.5
+        y = np.linspace(-sy/2, sy/2, ny, endpoint=False) + 0.5
+        z = np.linspace(-sz/2, sz/2, nz, endpoint=False) + 0.5
+        x, y, z = np.meshgrid(x, y, z, indexing='ij')
+        self.vox_centers = np.array([x.ravel(), y.ravel(), z.ravel()])
+        self.vox_origin = np.array([x.min(), y.min(), z.min()])
+    
+        # now set up detector grid
+        nx, nz = self.det_shape
+        sx, sz = self.det_size
+        x = np.linspace(-sx/2, sx/2, nx, endpoint=False) + 0.5
+        z = np.linspace(-sz/2, sz/2, nz, endpoint=False) + 0.5
+        xd, zd = np.meshgrid(x, z, indexing='ij')
+        y_source = -sy
+        y_det = sy
+        
+        # source and detector positions are needed for ray based method
+        self.source_centers = np.array([xd.ravel(), y_source*np.ones((self.n_det, )), zd.ravel()])
+        self.det_centers = np.array([xd.ravel(), y_det*np.ones((self.n_det, )), zd.ravel()])
+        
+        # this information is needed for voxel based method
+        self.det_orig = np.array([x.min(), y.min(), z.min()])
+        sx, sz = float(self.vox_shape[0]/self.det_shape[0]), float(self.vox_shape[2]/self.det_shape[1])
+        self.factor = np.array([sx, 1., sz])

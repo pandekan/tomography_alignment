9a6ff544acf99587a7a1c41880c1c8182de17d99
Kanupriya Pande <kanupriyap@gmail.com>
2022-05-11 11:03:01 -0700

    added functions for testing xz alignment

--- align/align_rigid_body.py
+++ align/align_rigid_body.py
@@ -60,7 +60,7 @@ def cost(parameters, align_obj, recon, projection, angles_start, xyz_start, cor_
     """
     
     xyz = xyz_start + parameters[:3] * align_obj.align_what[:3]
-    angles = angles_start[3:] + parameters[3:] * align_obj.align_what[3:]
+    angles = angles_start + parameters[3:] * align_obj.align_what[3:]
     
     cost = align_obj.cost(recon, projection, angles, xyz, cor_shift)
     
@@ -85,7 +85,7 @@ def gradient(parameters, align_obj, recon, projection, angles_start, xyz_start, 
     """
     
     xyz = xyz_start + parameters[:3] * align_obj.align_what[:3]
-    angles = angles_start[3:] + parameters[3:] * align_obj.align_what[3:]
+    angles = angles_start + parameters[3:] * align_obj.align_what[3:]
     
     cost, grad = align_obj.gradient(recon, projection, angles, xyz, cor_shift)
     grad = np.array(align_obj.align_what)[:, np.newaxis] * grad
@@ -94,5 +94,39 @@ def gradient(parameters, align_obj, recon, projection, angles_start, xyz_start, 
         return grad.T
     
     grad = np.dot(grad, cost)
+    
+    return grad
+
+def cost_xz(parameters, align_obj, rec, projection, angles_in, xyz_in, cor_shift, return_vector=False):
+
+    translations = np.array([xyz_in[0]+parameters[0], xyz_in[1], xyz_in[2]+parameters[1]])
+
+    cost = align_obj.cost(rec, projection, angles_in, translations, cor_shift)
+
+    if return_vector:
+        return cost
+
+    return 0.5 * np.linalg.norm(cost) ** 2
+
+
+def gradient_xz(parameters, align_obj, rec, projection, angles_in, xyz_in, cor_shift, return_vector=False):
+
+    translations = np.array([xyz_in[0]+parameters[0], xyz_in[1], xyz_in[2]+parameters[1]])
+
+    residual, s = align_obj.gradient(rec, projection, angles_in, translations, cor_shift)
+
+    vary_parameter = np.array([True, False, True, False, False, False])
+    s = s[vary_parameter]
+
+    #if scale_factor is None:
+    #    scale_factor = np.array([1.0, 1.0])
+
+    #s = s * scale_factor[:, np.newaxis]
+
+    if return_vector:
+        return s.T
+
+    grad = np.dot(s, residual)
+    print(translations[0], translations[2], 0.5*np.linalg.norm(residual)**2, grad)
     
     return grad

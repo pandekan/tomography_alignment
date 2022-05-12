6f2f74e7f7f64fc236493266d92a2dfff7a79b1b
Kanupriya Pande <kanupriyap@gmail.com>
2022-05-12 15:50:33 -0700

    cist and gradient functions return values as double precision since L-BFGS-B requires that; BFGS works with single precisiongit add align/align_rigid_body.py

--- align/align_rigid_body.py
+++ align/align_rigid_body.py
@@ -62,13 +62,14 @@ def cost(parameters, align_obj, recon, projection, angles_start, xyz_start, cor_
     xyz = xyz_start + parameters[:3] * align_obj.align_what[:3]
     angles = angles_start + parameters[3:] * align_obj.align_what[3:]
     
-    cost = align_obj.cost(recon, projection, angles, xyz, cor_shift)
+    residual = align_obj.cost(recon, projection, angles, xyz, cor_shift)
     
     if return_vector:
-        return cost
+        return residual.astype(np.float64)
     
-    return 0.5 * np.linalg.norm(cost)**2
+    residual = 0.5 * np.linalg.norm(residual)**2
 
+    return residual.astype(np.float64)
 
 def gradient(parameters, align_obj, recon, projection, angles_start, xyz_start, cor_shift,
              return_vector=False):
@@ -87,46 +88,16 @@ def gradient(parameters, align_obj, recon, projection, angles_start, xyz_start, 
     xyz = xyz_start + parameters[:3] * align_obj.align_what[:3]
     angles = angles_start + parameters[3:] * align_obj.align_what[3:]
     
-    cost, grad = align_obj.gradient(recon, projection, angles, xyz, cor_shift)
+    residual, grad = align_obj.gradient(recon, projection, angles, xyz, cor_shift)
     grad = np.array(align_obj.align_what)[:, np.newaxis] * grad
     
+    residual = residual.astype(np.float64)
+    grad = grad.astype(np.float64)
+
     if return_vector:
         return grad.T
     
-    grad = np.dot(grad, cost)
+    grad = np.dot(grad, residual)
     
     return grad
 
-def cost_xz(parameters, align_obj, rec, projection, angles_in, xyz_in, cor_shift, return_vector=False):
-
-    translations = np.array([xyz_in[0]+parameters[0], xyz_in[1], xyz_in[2]+parameters[1]])
-
-    cost = align_obj.cost(rec, projection, angles_in, translations, cor_shift)
-
-    if return_vector:
-        return cost
-
-    return 0.5 * np.linalg.norm(cost) ** 2
-
-
-def gradient_xz(parameters, align_obj, rec, projection, angles_in, xyz_in, cor_shift, return_vector=False):
-
-    translations = np.array([xyz_in[0]+parameters[0], xyz_in[1], xyz_in[2]+parameters[1]])
-
-    residual, s = align_obj.gradient(rec, projection, angles_in, translations, cor_shift)
-
-    vary_parameter = np.array([True, False, True, False, False, False])
-    s = s[vary_parameter]
-
-    #if scale_factor is None:
-    #    scale_factor = np.array([1.0, 1.0])
-
-    #s = s * scale_factor[:, np.newaxis]
-
-    if return_vector:
-        return s.T
-
-    grad = np.dot(s, residual)
-    print(translations[0], translations[2], 0.5*np.linalg.norm(residual)**2, grad)
-    
-    return grad
